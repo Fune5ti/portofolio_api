5a71d8b0cbfc876e9301c9f4112498b956f63fea
Daniel <daniel.pires@devgo.org>
2021-05-17 15:19:40 -0100

    bug: relocate folders to /src

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const databaseConnection = require("../config/database");
-const routes = require("../routes");
+const routes = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 3333;
